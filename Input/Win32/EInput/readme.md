d0833aa3045292029ca8c35ed8e1d97564de6f10
lute sasaki <gyaboyan@gmail.com>
2014-07-27 14:55:40 +0900

    Rename EInput.

--- Input/Win32/EInput/readme.md
+++ Input/Win32/EInput/readme.md
@@ -1,2 +1,2 @@
-Tenuki Input
+EInput Input
 ============
--- Input/Win32/EInput/src/main.cpp
+++ Input/Win32/EInput/src/main.cpp
@@ -4,25 +4,25 @@
 
 int main() {
 	EInput ei;
-  while(!ei.Get("Escape")) {
-  	const char *name[] = {
-  		"Up"    ,
-  		"Down"  ,
-  		"Left"  ,
-  		"Right" ,
-  		"B0"    ,
-  		"B1"    ,
-  		0,
-  	};
-  	int index = 0;
-  	while(name[index])
-	{
-		const char *str =name[index];
-    printf("[%02X:%s]", ei.Get(str), str);
-		index++;
+	while(!ei.Get("Escape")) {
+		const char *name[] = {
+			"Up"    ,
+			"Down"  ,
+			"Left"  ,
+			"Right" ,
+			"B0"    ,
+			"B1"    ,
+			0,
+		};
+		int index = 0;
+		while(name[index])
+		{
+			const char *str =name[index];
+			printf("[%02X:%s]", ei.Get(str), str);
+			index++;
+		}
+		printf("\n");
+		Sleep(15);
 	}
-  	printf("\n");
-    Sleep(15);
-  }
-  return 0;
+	return 0;
 }
