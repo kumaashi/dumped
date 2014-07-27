43db3fc86719bfebe6489f64a9c235ee4acb83e4
lute sasaki <gyaboyan@gmail.com>
2014-07-27 14:53:26 +0900

    Added easy input(EInput)

---
+++ Input/Win32/EInput/src/einput.h
@@ -0,0 +1,59 @@
+#ifndef _EINPUT_H_
+#define _EINPUT_H_
+
+#include <windows.h>
+#include <map>
+#include <string>
+
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "user32.lib")
+
+struct EInput
+{
+	std::map<std::string, bool> State;
+
+	enum
+	{
+		PWM_P   =    0xBFFF,
+		PWM_N   =    0x3FFF,
+		KB0     =    'Z',
+		KB1     =    'X',
+	};
+
+	void Update()
+	{
+		State.clear();
+		State["Escape"] = GetKeyInput(VK_ESCAPE) ? true : false;
+		State["Up"]     = GetKeyInput(VK_UP)     ? true : false;
+		State["Down"]   = GetKeyInput(VK_DOWN)   ? true : false;
+		State["Left"]   = GetKeyInput(VK_LEFT)   ? true : false;
+		State["Right"]  = GetKeyInput(VK_RIGHT)  ? true : false;
+		State["B0"]     = GetKeyInput(KB0)       ? true : false;
+		State["B1"]     = GetKeyInput(KB1)       ? true : false;
+		
+		JOYINFO JoyInfo;
+		if(joyGetPos(JOYSTICKID1, &JoyInfo) == JOYERR_NOERROR)
+		{
+			State["Up"]     = (JoyInfo.wYpos    < PWM_N)       ? true : false;
+			State["Down"]   = (JoyInfo.wYpos    > PWM_P)       ? true : false;
+			State["Left"]   = (JoyInfo.wXpos    < PWM_N)       ? true : false;
+			State["Right"]  = (JoyInfo.wXpos    > PWM_P)       ? true : false;
+			State["B0"]     = (JoyInfo.wButtons & JOY_BUTTON1) ? true : false;
+			State["B1"]     = (JoyInfo.wButtons & JOY_BUTTON2) ? true : false;
+		}
+	}
+
+	int GetKeyInput(int x)
+	{
+		return (GetAsyncKeyState(x) & 0x8000);
+	}
+	
+	bool Get(const char *name, int isupdate = 1)
+	{
+		if(isupdate) Update();
+		return State[name];
+	}
+};
+
+#endif _EINPUT_H_
+
---
+++ Input/Win32/EInput/src/main.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "einput.h"
+
+
+int main() {
+	EInput ei;
+  while(!ei.Get("Escape")) {
+  	const char *name[] = {
+  		"Up"    ,
+  		"Down"  ,
+  		"Left"  ,
+  		"Right" ,
+  		"B0"    ,
+  		"B1"    ,
+  		0,
+  	};
+  	int index = 0;
+  	while(name[index])
+	{
+		const char *str =name[index];
+    printf("[%02X:%s]", ei.Get(str), str);
+		index++;
+	}
+  	printf("\n");
+    Sleep(15);
+  }
+  return 0;
+}
