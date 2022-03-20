e0654d5d7eea5f1a23cd5227ec1f85462af451cf
lute-gyabo-yasai <gyaboyan@gmail.com>
2022-03-20 23:23:50 +0900

    added ffmpeg batch for twitter

---
+++ ffmpeg/batch/tomp4.bat
@@ -0,0 +1,1 @@
+ffmpeg -i %1 -vcodec libx264 -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -pix_fmt yuv420p -strict experimental -r 30 -t 2:20 -acodec aac -vb 1024k -minrate 1024k -maxrate 1024k -bufsize 1024k -ar 44100 -ac 2 out.mp4
